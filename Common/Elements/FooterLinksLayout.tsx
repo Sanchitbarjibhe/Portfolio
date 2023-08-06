2fb544d421a0029113c427659b2e7654fd351cd5
sanchitBarjibhe <barjibhe99@gmail.com>
2023-08-06 20:12:51 +0530

    production error slove

--- Common/Elements/FooterLinksLayout.tsx
+++ Common/Elements/FooterLinksLayout.tsx
@@ -9,10 +9,10 @@ interface FooterLinkProps {
 const FooterLinksLayout: React.FC<FooterLinkProps> = ({ linksTitle, href, }) => {
     return (
         <Link
-            variant="h6"
+            variant="subtitle1"
             href={href}
             target="_blank" rel="noopener noreferrer"
-            sx={{ flexGrow: 1, cursor: 'pointer', color: 'black', fontSize: '14px' }}
+            sx={{ flexGrow: 1, cursor: 'pointer', textDecoration: 'none', color: 'black', fontSize: '14px' }}
         >
             {linksTitle}
         </Link>
